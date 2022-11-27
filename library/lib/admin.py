f9f4b33a3f1453908e8261ce353096819d3b9e6a
Andrey <yurasov710@gmail.com>
2022-11-27 15:29:00 +0200

    made models

--- library/lib/admin.py
+++ library/lib/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
--- library/lib/models.py
+++ library/lib/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128, blank=True, null=True)
+    born = models.DateField(blank=True, null=True)
+    portrait = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+    
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='books')
+    summary = models.TextField(blank=True, null=True)
+    cover = models.URLField(blank=True, null=True)
+    genres = models.ManyToManyField(Genre, related_name='books', blank=True)
+    
+    def __str__(self):
+        return self.title
+    
